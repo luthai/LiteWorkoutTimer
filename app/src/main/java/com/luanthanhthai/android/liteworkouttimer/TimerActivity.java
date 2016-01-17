800336b2d2700e9b85f42934bb82b16aedfab697
Luan <luan086@gmail.com>
2016-01-17 22:00:31 +0100

    Properly implemented fragments with toolbar and menu

--- app/src/main/java/com/luanthanhthai/android/liteworkouttimer/TimerActivity.java
+++ app/src/main/java/com/luanthanhthai/android/liteworkouttimer/TimerActivity.java
@@ -1,42 +1,16 @@
 package com.luanthanhthai.android.liteworkouttimer;
 
-import android.os.Bundle;
+
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
+
 
 /**
  * Created by Thai on 15.01.2016.
  */
-public class TimerActivity extends AppCompatActivity {
-
-    private Toolbar mToolbar;
+public class TimerActivity extends SingleFragmentActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_timer);
-
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        if (mToolbar != null) {
-            setSupportActionBar(mToolbar);
-        }
-
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-        if (fragment == null) {
-            fragment = new TimerFragment();
-            fm.beginTransaction()
-                    .add(R.id.fragment_container, fragment)
-                    .commit();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_timer, menu);
-        return true;
+    protected Fragment createFragment() {
+        return TimerFragment.newInstance();
     }
 }
--- app/src/main/java/com/luanthanhthai/android/liteworkouttimer/TimerFragment.java
+++ app/src/main/java/com/luanthanhthai/android/liteworkouttimer/TimerFragment.java
@@ -1,20 +1,79 @@
 package com.luanthanhthai.android.liteworkouttimer;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 /**
  * Created by Thai on 15.01.2016.
  */
 public class TimerFragment extends Fragment {
 
+    private Toolbar mToolbar;
+
+    public static TimerFragment newInstance() {
+        return new TimerFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_timer, container, false);
+
+        mToolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.setSupportActionBar(mToolbar);
+
+        // For backwards compatibility set this
+        // near the end
+        setHasOptionsMenu(true);
+
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_timer, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Context context = getActivity();
+        CharSequence text;
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast;
+
+        switch (item.getItemId()) {
+            case R.id.menu_ic_create_routine:
+                text = "Create routine";
+                toast = Toast.makeText(context, text, duration);
+                toast.show();
+
+                return true;
+            case R.id.menu_ic_settings:
+                text = "Settings";
+                toast = Toast.makeText(context, text, duration);
+                toast.show();
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
